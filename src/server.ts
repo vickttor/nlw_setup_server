5be3b079c82af7cbbbaea0c40ea44f676d5ae797
Victor Silva <victor.silvaPC@hotmail.com>
2023-01-18 18:57:01 -0300

    :zap: feat: added relations in table | created the rest of the table and some routes

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,17 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+import { appRoutes } from "./routes";
 
 const app = Fastify()
-const prisma = new PrismaClient() // instantiating the prisma ORM
 const PORT = 3333 || process.env.PORT
 
 app.register(cors) // Applying access policies to app
 
-// GET ROUTE
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Concluir'
-      }
-    }
-  })
-
-  return habits
-})
+app.register(appRoutes) // Consuming Routes
 
 // Making the Server listen the 3333 (or server) port
 app.listen({
   port: PORT,
 }).then(()=> {
-  console.log("HTTP server running in " + PORT)
+  console.log("HTTP server running in http://localhost:" + PORT)
 })
